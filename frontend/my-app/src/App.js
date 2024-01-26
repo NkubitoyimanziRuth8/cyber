4c79afeac6e8331344a1ccb2e068d04b110ef21e
NkubitoyimanziRuth8 <NkubitoyimaniRuth8nkubitoyimanzirut2@gmail.com>
2024-01-26 07:48:28 +0200

    connection added

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -19,7 +19,7 @@ function App() {
   const handleLogin = async () => {
     try {
       // Send the form data to the backend for storage
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch(`{process.env.REACT_APP_BACKEND_URL}/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
